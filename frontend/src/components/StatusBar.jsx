92bca81976e1ca3fa8bd9a783e14bd455b933bde
sungchulJE <saintiron82@gmail.com>
2026-02-26 02:48:50 +0900

    chore: version bump to v0.6.1.20260226_02

--- frontend/src/components/StatusBar.jsx
+++ frontend/src/components/StatusBar.jsx
@@ -351,7 +351,7 @@ const StatusBar = ({
                 <span className="text-[9px] text-gray-600 font-mono mr-2">
                     {typeof __APP_VERSION__ !== 'undefined' && typeof __BUILD_ID__ !== 'undefined'
                         ? `v${__APP_VERSION__}.${__BUILD_ID__}`
-                        : 'v0.6.1.20260226_01'}
+                        : 'v0.6.1.20260226_02'}
                 </span>
                 <div className="flex-shrink-0">
                     {isOpen ? <X size={14} /> : t('label.show_logs')}
